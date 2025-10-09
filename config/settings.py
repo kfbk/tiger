9ac22bc764a73d26d9d2ece365f2a29ecb3a3b50
kfbk <satou1497@gmail.com>
2025-10-09 15:28:35 +0900

    2nd commit

--- config/settings.py
+++ config/settings.py
@@ -129,4 +129,6 @@ import os
 IMAGE_URL = '/images/'
 IMAGE_ROOT = os.path.join(BASE_DIR, 'images')
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATICFILES_DIRS = [BASE_DIR / 'static']
